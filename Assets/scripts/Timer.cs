55b2826ebc0c0fd7a091ae2b8a431b244d7d4255
NICOLAS COLLARES ELPO <nicolas_elpo@estudante.sesisenai.org.br>
2025-12-03 11:03:56 -0300

    timer novo (talvez seja útil pra mecânica de bonus)

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,16 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] TextMeshProUGUI textoTimer;
+    [SerializeField] float tempoRestante;
 
     // Update is called once per frame
     void Update()
     {
-        
+     if (tempoRestante > 0)
+        {
+            tempoRestante = tempoRestante - Time.deltaTime;
+
+        }
+    
+    else
+    {
+    tempoRestante = 0;
+    //CabouOJogo();
+    textoTimer.color = Color.yellow;
+
+    }
+int minutos = Mathf.FloorToInt(tempoRestante / 60);
+int segundos = Mathf.FloorToInt(tempoRestante % 60);
+textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+
+
     }
 }
